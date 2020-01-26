7517366f20c38a4a05aa7c13b99de70bb1f94ce0
jiangtt18 <tingtingjiang18@gmail.com>
2020-01-25 20:22:34 -0800

    tested code for store

---
+++ frontend/reducers/todos_reducer.js
@@ -0,0 +1,13 @@
+const todoReducer = (state={}, action) => {
+    Object.freeze(state);
+    switch(action.type){
+        // case :
+        //     break;
+        // case:
+        //     break;
+        default:
+            return state;
+    }
+};
+
+export default todoReducer;
